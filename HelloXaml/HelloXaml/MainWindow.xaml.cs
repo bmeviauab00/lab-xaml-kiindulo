4ecf38ec65cf00d7e9999b3bbd199e95b09ed61c
Zoltán Benedek <bzolka@aut.bme.hu>
2024-03-15 10:14:13 +0100

    Kiinduló keretbe gomb vissza

--- HelloXaml/HelloXaml/MainWindow.xaml.cs
+++ HelloXaml/HelloXaml/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 
 namespace HelloXaml;
 
@@ -7,5 +8,10 @@ public sealed partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void myButton_Click(object sender, RoutedEventArgs e)
+    {
+        myButton.Content = "Clicked";
     }
 }
